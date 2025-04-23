46bb774a364d4a3d6958a46daac58f859a2c4156
npessina1 <pessina.nicolas.pro@gmail.com>
2025-04-23 16:13:46 +0200

    Rangement du code

--- src/moduleVisualisation/communication.cpp
+++ src/moduleVisualisation/communication.cpp
@@ -7,12 +7,9 @@ Communication::Communication()
 Communication::~Communication()
 {
 }
+/***********Etablir connexions***********/
 
-void Communication::envoyerTrame(const QString& destinataire,
-                                 const QString& trame)
-{
-}
-
+/***********Traiter trames***********/
 void Communication::traiterTrame()
 {
     const QString trame;
@@ -29,18 +26,82 @@ void Communication::traiterTrame()
         }
         else if(estDemandeChangementPage(trame))
         {
-            gererChangementPage()
+            gererChangementPage();
         }
         else
         {
+            gererChangementPage();
         }
     }
 }
 
+bool Communication::estTrameValide(const QString& trame)
+{
+    if(trame.startsWith(carDebutTrame) && trame.endsWith(carFinTrame))
+        return true;
+    return false;
+}
+
+bool Communication::verifierTypeTrame(const QString& trame,
+                                      const QString& caractere)
+{
+    if(trame.contains(caractere))
+        return true;
+    return false;
+}
+
+bool Communication::estTrameConfiguration(const QString& trame)
+{
+    if(verifierTypeTrame(trame, carConfiguration))
+        return true;
+    return false;
+}
+
+bool Communication::estTrameFinDePartie(const QString& trame)
+{
+    if(verifierTypeTrame(trame, carFinPartie))
+        return true;
+    return false;
+}
+
+bool Communication::estTrameAssociation(const QString& trame)
+{
+    if(verifierTypeTrame(trame, carAssociation))
+        return true;
+    return false;
+}
+
+bool Communication::estDemandePageAccueil(const QString& trame)
+{
+    if(verifierTypeTrame(trame, carPageAccueil))
+        return true;
+    return false;
+}
+
+bool Communication::estDemandePageHistorique(const QString& trame)
+{
+    if(verifierTypeTrame(trame, carPageHistorique))
+        return true;
+    return false;
+}
+
+bool Communication::estDemandeChangementPage(const QString& trame)
+{
+    if(estDemandePageAccueil(trame) || estDemandePageHistorique(trame))
+        return true;
+    return false;
+}
+
+/***********Envoyer trames***********/
 QString Communication::construireTrame(const QString& message)
 {
     QString trame = carDebutTrame + message + carFinTrame;
     return trame;
+}
+
+void Communication::envoyerTrame(const QString& destinataire,
+                                 const QString& trame)
+{
 }
 
 void Communication::demanderAssociation(const QString& destinataire)
@@ -67,59 +128,15 @@ void Communication::signalerFinDePartie(const QString& destinataire)
     envoyerTrame(destinataire, construireTrame(carFinPartie));
 }
 
-bool Communication::estTrameConfiguration(const QString& trame)
+/***********Gestion***********/
+void Communication::gererConfiguration()
 {
-    if(verifierTypeTrame(trame, carConfiguration))
-        return true;
-    return false;
 }
 
-bool Communication::estTrameFinDePartie(const QString& trame)
+void Communication::gererAssociation()
 {
-    if(verifierTypeTrame(trame, carFinPartie))
-        return true;
-    return false;
 }
 
-bool Communication::estDemandePageAccueil(const QString& trame)
+void Communication::gererChangementPage()
 {
-    if(verifierTypeTrame(trame, carPageAccueil))
-        return true;
-    return false;
-}
-
-bool Communication::estDemandePageHistorique(const QString& trame)
-{
-    if(verifierTypeTrame(trame, carPageHistorique))
-        return true;
-    return false;
-}
-
-bool Communication::estTrameAssociation(const QString& trame)
-{
-    if(verifierTypeTrame(trame, carAssociation))
-        return true;
-    return false;
-}
-
-bool Communication::verifierTypeTrame(const QString& trame,
-                                      const QString& caractere)
-{
-    if(trame.contains(caractere))
-        return true;
-    return false;
-}
-
-bool Communication::estTrameValide(const QString& trame)
-{
-    if(trame.startsWith(carDebutTrame) && trame.endsWith(carFinTrame))
-        return true;
-    return false;
-}
-
-bool Communication::estDemandeChangementPage(const QString& trame)
-{
-    if(estDemandePageAccueil(trame) || estDemandePageHistorique(trame))
-        return true;
-    return false;
 }
--- src/moduleVisualisation/communication.h
+++ src/moduleVisualisation/communication.h
@@ -1,10 +1,24 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+#include <QLowEnergyController>
 #include <QStringList>
 #include <QString>
 
-class Communication
+//// adresse du module de configuration////
+#define MODULECONFIGURATION = "";
+
+//// adresse des modules de détection de balle////
+#define TABLE1 = "";
+#define TABLE2 = "";
+#define TABLE3 = "";
+#define TABLE4 = "";
+#define TABLE5 = "";
+#define TABLE6 = "";
+#define TABLE7 = "";
+#define TABLE8 = "";
+
+class Communication : public QObject
 {
   public:
     /*enum TypeTrame
@@ -22,40 +36,45 @@ class Communication
   public:
     Communication();
     ~Communication();
-    void    envoyerTrame(const QString& destinataire, const QString& trame);
-    void    traiterTrame();
+
+    //////Etablir connexions//////
+
+    //////Traiter trames//////
+    void traiterTrame();
+    bool estTrameValide(const QString& trame);
+    bool verifierTypeTrame(const QString& trame, const QString& caractere);
+    bool estTrameConfiguration(const QString& trame);
+    bool estTrameFinDePartie(const QString& trame);
+    bool estTrameAssociation(const QString& trame);
+    bool estDemandePageAccueil(const QString& trame);
+    bool estDemandePageHistorique(const QString& trame);
+    bool estDemandeChangementPage(const QString& trame);
+
+    //////Envoyer trames//////
     QString construireTrame(const QString& trame);
+    void    envoyerTrame(const QString& destinataire, const QString& trame);
     void    demanderAssociation(const QString& destinataire);
     void    confirmerAssociation(const QString& destinataire);
     void    envoyerModeDeJeu(const QString& destinataire,
                              const QString& modeDeJeu);
     void    envoyerDebutDePartie(const QString& destinataire);
     void    signalerFinDePartie(const QString& destinataire);
-    bool    estTrameConfiguration(const QString& trame);
-    bool    estTrameFinDePartie(const QString& trame);
-    bool    estDemandePageAccueil(const QString& trame);
-    bool    estDemandePageHistorique(const QString& trame);
-    bool    estTrameAssociation(const QString& trame);
-    bool    verifierTypeTrame(const QString& trame, const QString& caractere);
-    bool    estTrameValide(const QString& trame);
-    bool    estDemandeChangementPage(const QString& trame);
-    void    gererConfiguration();
-    void    gererAssociation();
+
+    //////Gestion//////
+    void gererConfiguration();
+    void gererAssociation();
+    void gererChangementPage();
 
   private:
-    QStringList       trames;
-    const QString     carDebutTrame      = "$";
-    const QString     carFinTrame        = "\n";
-    const QString     carConfiguration   = "C";
-    const QString     carDebutPartie     = "D";
-    const QString     carFinPartie       = "F";
-    const QString     carPageAccueil     = "S0";
-    const QString     carPageHistorique  = "S1";
-    const QString     carAssociation     = "A";
-    const QStringList moduleDetection[8] = {
-        "", "", "", "", "", "", "", ""
-    }; // adresse des modules de détection de balle
-    const QString moduleConfiguration = ; // adresse du module de configuration
+    QStringList   trames;
+    const QString carDebutTrame     = "$";
+    const QString carFinTrame       = "\n";
+    const QString carConfiguration  = "C";
+    const QString carDebutPartie    = "D";
+    const QString carFinPartie      = "F";
+    const QString carPageAccueil    = "S0";
+    const QString carPageHistorique = "S1";
+    const QString carAssociation    = "A";
 };
 
 #endif // COMMUNICATION_H
