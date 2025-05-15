c889d025c25665961bdc74edd8daa6d9ae5d4191
tvaira <tvaira@free.fr>
2025-05-15 23:34:28 +0200

    Restructure le code et ajoute la gestion des modes de jeu

--- simulateur/lib/bluetooth/bluetooth.h
+++ simulateur/lib/bluetooth/bluetooth.h
@@ -1,6 +1,7 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
+#include "../../include/jolly-jumpi.h"
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 #include <esp_bt_main.h>
@@ -9,7 +10,12 @@
 #include <esp_spp_api.h>
 #include <esp_log.h>
 
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled
+#endif
+
 #define ATTENTE_PRET (10 * 1000)
+#define BUFFER_TRAME 64 //!< Buffer pour les trames
 
 void trouverPeripherique(BTAdvertisedDevice* peripherique);
 void demanderConfirmation(uint32_t numVal);
@@ -33,5 +39,11 @@ int getPeripheriquesAppaires(int nb, esp_bd_addr_t* listePeripheriquesAppaires);
 bool supprimerAppairagePeripherique(uint8_t* adressePeripherique);
 void supprimerPeripheriquesAppaires();
 // void getNomPeripherique(uint8_t adresseDistante[]);
+
+bool      lireTrame(String& trame);
+TypeTrame verifierTrame(String& trame);
+String    extraireChamp(const String& trame, unsigned int numeroChamp);
+void      envoyerTrameTir(int numeroTable, int score);
+void      envoyerTrameAssociation();
 
 #endif // BLUETOOTH_H
